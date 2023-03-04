7f2837e453d8928244c2e7c50351be16ff034f54
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-04 11:18:48 +0100

    added global style

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Theme from "./css/styles/theme";
+import GlobalStyle from './scss/global/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <Theme> */}
+    <GlobalStyle />
     <App />
+    {/* </Theme> */}
   </React.StrictMode>
 );
 
