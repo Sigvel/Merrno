6109aa303aff6edf99207ad0a921cca6ebb5a71c
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-05 11:35:15 +0100

    initial footer

---
+++ src/components/layout/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer () {
+    return (
+        <div>
+            <span>Title of the page</span>
+            <button>
+                arrow left
+            </button>
+            <button>
+                arrow right
+            </button>
+        </div>
+    )
+}
+
+export default Footer
