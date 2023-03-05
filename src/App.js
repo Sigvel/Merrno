3f5bfd404223ecc8abd0585eb723479259a2cacc
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-05 11:34:47 +0100

    routes and paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Contact from "./pages/contact";
+import Cart from "./pages/cart";
+import Layout from "./components/layout/Layout";
+import RouteNotFound from "./pages/error";
+import Product from "./pages/product";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>Hello World</div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="product/:id" element={<Product />} />
+          <Route path="cart/" element={<Cart />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
