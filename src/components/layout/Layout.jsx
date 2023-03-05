7b4035b684f4687b0f78b0c514849f650fee84ac
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-05 12:41:32 +0100

    removed footer from layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function Layout() {
@@ -7,7 +6,6 @@ function Layout() {
         <div>
             <Header />
             <Outlet />
-            <Footer />
         </div>
     )
 }
