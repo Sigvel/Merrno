1624680af5f89371997e0a7d46f20ce9e3e2c8bd
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-05 11:35:22 +0100

    initial header

---
+++ src/components/layout/components/Header.jsx
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+
+
+function Header() {
+    return (
+        <header>
+            <div>Merrno</div>
+            <Nav />
+        </header>
+    )
+};
+
+
+export default Header
