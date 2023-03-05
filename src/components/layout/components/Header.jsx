fd0c7192a20ba453a8d2ef7bfad9827b7a240c50
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-05 20:34:59 +0100

    header styling

--- src/components/layout/components/Header.jsx
+++ src/components/layout/components/Header.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
-
 function Header() {
-    return (
-        <header>
-            <Link to="/">Merrno</Link>
-            <Nav />
-        </header>
-    )
-};
+  return (
+    <header>
+      <div className="element-border">
+        <Link to="/" className="logo">
+          Merrno
+        </Link>
+      </div>
+      <Nav />
+    </header>
+  );
+}
 
-
-export default Header
+export default Header;
--- src/components/layout/components/Nav.jsx
+++ src/components/layout/components/Nav.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import SearchIcon from "../../../assets/interface/navigation/icons8-search-48.png"
+import ContactIcon from "../../../assets/interface/navigation/icons8-headset-48.png"
+import CartIcon from "../../../assets/interface/navigation/icons8-shopping-bag-48.png"
+
 
 function Nav() {
     return (
         <nav>
-            <form>
-                <label>Button (border, logo)</label>
+            <form className="element-border">
+                <label><img src={SearchIcon} alt="Search Icon" /></label>
                 <input type="search" placeholder="Search"></input>
             </form>
             <ul>
-                <li>
-                    <Link to="/contact">Contact</Link>
+                <li className="element-border">
+                    <Link to="/contact"><img src={ContactIcon} alt="Contact Icon" />Contact</Link>
                 </li>
-                <li>
-                    <Link to="/cart">Cart</Link>
+                <li className="element-border">
+                    <Link to="/cart"><img src={CartIcon} alt="Cart Icon" />Cart</Link>
                 </li>
             </ul>
         </nav>
