b7bbce8b8a53ab0416044d41efa529849190cf69
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-05 12:42:00 +0100

    added logo as link to homepage

--- src/components/layout/components/Header.jsx
+++ src/components/layout/components/Header.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
 
 function Header() {
     return (
         <header>
-            <div>Merrno</div>
+            <Link to="/">Merrno</Link>
             <Nav />
         </header>
     )
