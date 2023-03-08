8830e9a551f7d1aadb48ca4210b583088e127b3e
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-08 15:56:02 +0100

    Updated card component

--- src/App.styles.js
+++ src/App.styles.js
@@ -3,13 +3,14 @@ import "./scss/components/variables.scss"
 
 export const Container = styled.div`
 display: grid;
-grid-template-columns: repeat(2, 1fr);
-grid-template-rows: repeat(2, minmax(100px, auto));
+grid-template-columns: repeat(1, 1fr);
+// grid-template-rows: repeat(2, minmax(100px, auto));
 gap: 1rem;
 padding: 1rem 1rem;
 border: solid 0.1rem white;
 border-radius: 0.5rem;
 box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+margin-bottom: 7.5rem;
 `
 
 export const Card = styled.div`
@@ -17,42 +18,6 @@ background: #508BAD;
 box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 border-radius: 1rem;
 color: black;
-
-display: flex;
-flex-direction: column;
-
-figure {
-    display: flex;
-    justify-content: center;
-    padding: 1rem;
-    img {
-        border-radius: 1rem;
-        width: 100%;
-        height: 9rem;
-    }
-}
-
-h2, h3 {
-margin: 0.2rem 1rem
-}
-
-h2 {
-    font-weight: var(--font-semibold)
-}
-
-h3 {
-    flex-grow: 1;
-}
-
-p {
-    margin: 1.5rem 1rem 1rem;
-    font-weight: 600;
-    color: white;
-}
-
-button {
-    margin: 0rem 1rem 1rem;
-}
 `;
 
 export const Button = styled.button`
@@ -65,7 +30,30 @@ background: black;
 color: white;
 `;
 
+export const PageContainer = styled.div`
+border: solid 0.1rem white;
+text-align: center;
+border-radius: 0.5rem;
+padding: 0.5rem 1rem;
+box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`
 
-export const Category = styled.div`
+export const Category = styled.ul`
+display: flex;
+overflow-x: scroll;
+border: 0.1rem solid white;
+border-radius: 0.5rem;
+background: linear-gradient(65deg, rgb(68, 124, 156), rgb(25, 55, 100));
+position: fixed;
+bottom: 0;
+left: 0.5rem;
+right: 0.5rem;
 
+img {
+    background: black;
+    padding: 0.5rem;
+    margin: 1rem 0.8rem;
+    width: 7rem;
+    border-radius: 0.5rem;
+}
 `
