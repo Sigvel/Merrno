d57610f8dd6b0060b52fb52760d56491827c6b0d
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-08 15:58:06 +0100

    price discount Component

---
+++ src/components/Price.jsx
@@ -0,0 +1,21 @@
+import "../scss/styles.scss"
+
+function ProductPrice({ price, discount }) {
+  console.log(`${price} and ${discount}`);
+
+  let sale;
+
+  if (price !== discount) {
+    sale = Math.floor(price - discount);
+  } else {
+    sale = false;
+  }
+
+  return (
+    <div>
+      <p className={sale ? "sale" : ""}>{price },-</p>{sale && <p>{discount} <span className="sale-color">-{sale},-</span></p>}
+    </div>
+  );
+}
+
+export default ProductPrice;
