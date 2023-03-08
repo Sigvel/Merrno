fc0c58e39e0f0a488b71dc828e4d2bd08373ddf9
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-08 15:58:18 +0100

    single product fetch component

---
+++ src/components/Product.jsx
@@ -0,0 +1,56 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import * as S from "../App.styles"
+import styles from "../scss/modules/Card.module.scss";
+
+const url = "https://api.noroff.dev/api/v1/online-shop/";
+
+function ProductFetch() {
+    const [product, setProduct] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const [isError, setIsError] = useState(null)
+
+    let { id } = useParams();
+
+    useEffect(() => {
+        async function getProduct(url) {
+            try {
+                setIsLoading(true);
+                setIsError(false);
+
+                const res = await fetch(url);
+                const json = await res.json();
+
+                setProduct(json)
+            } catch (err) {
+                console.log(err)
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        getProduct(`${url}${id}`);
+    }, [id])
+
+    if(isLoading || !product) {
+        return <div>Loading...</div>
+    }
+
+    if(isError) {
+        return <div>Error</div>
+    }
+
+    console.log(product);
+    return (
+        <S.Card className={styles.bigCard}>
+            <figure>
+          <img src={product.imageUrl} alt={product.title} />
+          </figure>
+          <div>{product.price}</div>
+          <h2>{product.title}</h2>
+          <h3>{product.description}</h3>
+        </S.Card>
+      );
+}
+
+export default ProductFetch;
