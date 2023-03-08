9a5671304f12422d6ffd2e9d7eee45bfffbcbb66
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-08 15:59:02 +0100

    added product Component to product specific page

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom";
+import ProductFetch from "../../components/Product";
+import * as S from "../../App.styles"
 
 function Product() {
     let params = useParams();
     console.log(params);
     // Logs the id of whichever product page you are on e.g.
     // {id: '1'} or {id: '2'}
-    return <div>Individual product page: {params.id}</div>;
+    return (
+    <S.PageContainer>
+      <ProductFetch id={params.id} />
+    </S.PageContainer>
+    )
   }
 
   export default Product
