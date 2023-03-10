6dff03eca062281ddd3ea384dad486dccf2320fc
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-10 17:37:15 +0100

    more general styles for styled components

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,59 +1,71 @@
-import styled from 'styled-components';
-import "./scss/components/variables.scss"
+import styled from "styled-components";
+import "./scss/components/variables.scss";
+
+export const PageContainer = styled.div`
+  border: solid 0.1rem white;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+  h1 {
+    text-align: center;
+    text-transform: uppercase;
+    position: fixed;
+    inset: auto 0.5rem 7.1rem;
+    background: linear-gradient(95deg, #b97f00, #6b5628);
+    border-radius: 0.5rem;
+    border: solid 0.1rem white;
+    border-bottom: none;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export const Container = styled.div`
-display: grid;
-grid-template-columns: repeat(1, 1fr);
-// grid-template-rows: repeat(2, minmax(100px, auto));
-gap: 1rem;
-padding: 1rem 1rem;
-border: solid 0.1rem white;
-border-radius: 0.5rem;
-box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-margin-bottom: 7.5rem;
-`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  // grid-template-rows: repeat(2, minmax(100px, auto));
+  gap: 1rem;
+`;
 
 export const Card = styled.div`
-background: #508BAD;
-box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-border-radius: 1rem;
-color: black;
+  background: #dc990a;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 1rem;
+  color: black;
 `;
 
 export const Button = styled.button`
-text-transform: uppercase;
-font-size: 1.2rem;
-padding: 0.3rem 1.5rem;
-border-style: none;
-border-radius: 3rem;
-background: black;
-color: white;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  padding: 0.3rem 1.5rem;
+  border-style: none;
+  border-radius: 3rem;
+  background: black;
+  color: white;
 `;
 
-export const PageContainer = styled.div`
-border: solid 0.1rem white;
-text-align: center;
-border-radius: 0.5rem;
-padding: 0.5rem 1rem;
-box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-`
-
 export const Category = styled.ul`
-display: flex;
-overflow-x: scroll;
-border: 0.1rem solid white;
-border-radius: 0.5rem;
-background: linear-gradient(65deg, rgb(68, 124, 156), rgb(25, 55, 100));
-position: fixed;
-bottom: 0;
-left: 0.5rem;
-right: 0.5rem;
+  display: flex;
+  overflow-x: scroll;
+  border: 0.1rem solid white;
+  border-radius: 0.5rem;
+  background: linear-gradient(95deg, #b97f00, #6b5628);
+  position: fixed;
+  inset: auto 0.5rem 0rem;
 
-img {
+  img {
     background: black;
     padding: 0.5rem;
     margin: 1rem 0.8rem;
-    width: 7rem;
+    width: 5rem;
     border-radius: 0.5rem;
-}
-`
+  }
+`;
+
+export const Breadcrumb = styled.ul`
+  display: flex;
+  li {
+    padding: 0.5rem;
+    background: black;
+  }
+`;
