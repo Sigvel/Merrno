03b73d8abef59555a8885660010a3c9d282f5f81
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-12 14:38:57 +0100

    imported new category icons

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,23 +2,23 @@
 import ProductsFetch from "../../components/GetProducts/index";
 import * as S from "../../App.styles"
 import CableIcon from "../../assets/interface/categories/icons8-audio-cable-96.png";
-import HangerIcon from "../../assets/interface/categories/icons8-hanger-96.png";
 import LaptopIcon from "../../assets/interface/categories/icons8-laptop-96.png";
 import SneakerIcon from "../../assets/interface/categories/icons8-sneakers-96.png";
-import WorkstationIcon from "../../assets/interface/categories/icons8-workstation-96.png";
-import ProcessorIcon from "../../assets/interface/categories/icons8-processor-96.png";
+import ElectricalIcon from "../../assets/interface/categories/icons8-electrical-96.png";
+import JewelryIcon from "../../assets/interface/categories/icons8-jewelry-96.png";
+import ClothesIcon from "../../assets/interface/categories/icons8-clothes-96.png";
 
 function Home() {
   return (
     <S.PageContainer>
       <ProductsFetch />
         <S.Category>
+        <img src={ElectricalIcon} alt="cable category" />
+        <img src={ClothesIcon} alt="cable category" />
           <img src={CableIcon} alt="cable category" />
-          <img src={HangerIcon} alt="cable category" />
           <img src={LaptopIcon} alt="cable category" />
           <img src={SneakerIcon} alt="cable category" />
-          <img src={WorkstationIcon} alt="cable category" />
-          <img src={ProcessorIcon} alt="cable category" />
+          <img src={JewelryIcon} alt="cable category" />
         </S.Category>
       </S.PageContainer>
   );
