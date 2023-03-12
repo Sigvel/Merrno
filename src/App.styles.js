dee6a6e05772d2b6e1c18c4470138169d7129752
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-12 13:19:50 +0100

    top right left to inset and padding change to page container

--- src/App.styles.js
+++ src/App.styles.js
@@ -4,15 +4,16 @@ import "./scss/components/variables.scss";
 export const PageContainer = styled.div`
   border: solid 0.1rem white;
   border-radius: 0.5rem;
-  padding: 0.5rem 1rem;
+  padding: 0.5rem 1rem 15rem;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 
   h1 {
     text-align: center;
     text-transform: uppercase;
     position: fixed;
-    inset: auto 0.5rem 7.1rem;
+    inset: auto 0.5rem 6.2rem;
     background: linear-gradient(95deg, #b97f00, #6b5628);
+    text-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     border-radius: 0.5rem;
     border: solid 0.1rem white;
     border-bottom: none;
@@ -42,6 +43,7 @@ export const Button = styled.button`
   border-radius: 3rem;
   background: black;
   color: white;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 `;
 
 export const Category = styled.ul`
@@ -50,6 +52,7 @@ export const Category = styled.ul`
   border: 0.1rem solid white;
   border-radius: 0.5rem;
   background: linear-gradient(95deg, #b97f00, #6b5628);
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   position: fixed;
   inset: auto 0.5rem 0rem;
 
@@ -57,7 +60,7 @@ export const Category = styled.ul`
     background: black;
     padding: 0.5rem;
     margin: 1rem 0.8rem;
-    width: 5rem;
+    width: 4rem;
     border-radius: 0.5rem;
   }
 `;
@@ -67,5 +70,6 @@ export const Breadcrumb = styled.ul`
   li {
     padding: 0.5rem;
     background: black;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   }
 `;
