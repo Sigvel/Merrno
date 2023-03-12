5a9c9106308f1f3469c1d4d8283d4f95dd25dda2
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-12 13:20:32 +0100

    removed page container around header

--- src/components/layout/components/Header/index.jsx
+++ src/components/layout/components/Header/index.jsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom";
 import Nav from "../Nav/index";
 import "./../../../../scss/components/header/header.scss";
-import * as S from "../../../../App.styles"
+// import * as S from "../../../../App.styles";
 
 function Header() {
   return (
     <header>
-      <S.PageContainer>
         <Link to="/" className="logo">
           Merrno
         </Link>
-      </S.PageContainer>
       <Nav />
     </header>
   );
