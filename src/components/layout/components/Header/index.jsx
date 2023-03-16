5651261e2d6df4150e222184d23eba123ad55337
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-16 18:42:46 +0100

    updated logo

--- src/components/layout/components/Header/index.jsx
+++ src/components/layout/components/Header/index.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
+
 import Nav from "../Nav/index";
 import "./../../../../scss/components/header/header.scss";
-// import * as S from "../../../../App.styles";
 
 function Header() {
   return (
     <header>
-        <Link to="/" className="logo">
-          Merrno
-        </Link>
-      <Nav />
+      <Link to="/" className="logo element-border">
+        <p>Merrno</p>
+      </Link>
+      <Nav/>
     </header>
   );
 }
