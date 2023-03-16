63aa1bb9549f74f421551a9487ce91895185e9cd
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-16 18:42:02 +0100

    responsive header up to 750px

--- src/App.styles.js
+++ src/App.styles.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import "./scss/components/variables.scss";
+import "./scss/components/_variables.scss";
 
 export const PageContainer = styled.div`
-  border: solid 0.1rem #676F9D;
+  border: solid 0.1rem #676f9d;
   border-radius: 0.5rem;
-  padding: 0.5rem 1rem 9rem;
+  padding: 9rem 1rem 10rem;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 
   h1 {
@@ -13,28 +13,49 @@ export const PageContainer = styled.div`
     font-weight: var(--font-bold);
     position: fixed;
     inset: auto 0.5rem 6rem;
-    // background: linear-gradient(95deg, #b97f00, #6b5628);
-    background: #2D3250;
+    background: #2d3250;
     text-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
     border-radius: 0.5rem;
-    border: solid 0.1rem #676F9D;
+    border: solid 0.1rem #676f9d;
     border-bottom: none;
-    padding: 0.5rem 1rem;
+    padding: 0.5rem;
+  }
+
+  @media (min-width: 530px) {
+    padding: 11rem 1rem 10rem;
+    h1 {
+      font-size: 2.5rem;
+      inset: auto 0.5rem 6.1rem;
+    }
   }
 `;
 
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(1, 1fr);
-  // grid-template-rows: repeat(2, minmax(100px, auto));
+  grid-template-rows: repeat(2, minmax(100px, auto));
   gap: 1rem;
+
+  @media (min-width: 370px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 530px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  // @media (min-width: 780px) {
+  // }
+
+  @media (min-width: 900px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `;
 
 export const Card = styled.div`
   background: #424669;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   border-radius: 1rem;
-  // color: black;
 `;
 
 export const Button = styled.button`
@@ -43,23 +64,22 @@ export const Button = styled.button`
   padding: 0.3rem 1.5rem;
   border-style: none;
   border-radius: 3rem;
-  background: #F8B179;
-  box-shadow: rgba(248,177,121, 0.35) 0px 5px 15px;
-  color: #2D3250;
+  background: #f8b179;
+  box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px;
+  color: #2d3250;
 `;
 
 export const Category = styled.ul`
   display: flex;
   overflow-x: scroll;
-  border: 0.1rem solid #676F9D;
-  // background: linear-gradient(95deg, #b97f00, #6b5628);
-  background: #2D3250;
+  border: 0.1rem solid #676f9d;
+  background: #2d3250;
   position: fixed;
   inset: auto 0.5rem 0;
 
   img {
-    background: #F8B179;
-    box-shadow: rgba(248,177,121, 0.35) 0px 5px 15px;
+    background: #f8b179;
+    box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px;
     padding: 0.5rem;
     margin: 1rem 0.8rem;
     width: 4rem;
@@ -74,4 +94,65 @@ export const Breadcrumb = styled.ul`
     background: black;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   }
+`;
+
+export const SearchBar = styled.div`
+display: flex;
+
+  input[type="search"] {
+    width: 100%;
+    padding: 0.25rem 0.7rem;
+    border-radius: 5rem;
+    border: none;
+    background: #F8B179;
+    color: #2D3250;
+    font-size: 1.2rem;
+    box-shadow: rgba(248,177,121, 0.35) 0px 5px 15px;
+    ::placeholder {
+      color: #2D3250;
+    }
+    :focus {
+      outline: #2D3250;
+    }
+  }
+
+  img {
+    border-radius: 5rem;
+    padding: 0.1rem 1rem;
+    width: 4rem;
+    background: #F8B179;
+    position: absolute;
+    right: 13.5rem;
+  }
+
+  @media (min-width: 530px) {
+    input[type="search"] {
+      width: 100%;
+      font-size: 1.5rem;
+      padding: 0.6rem 0.7rem;
+    }
+
+    img {
+      width: 7.2rem;
+      margin: 0.2rem 1rem;
+      padding: 0.2rem 2.4rem;
+      right: 23rem;
+    }
+  }
+`;
+
+export const SearchIcon = styled.div`
+img {
+  margin: 0 auto;
+  border-radius: 5rem;
+  padding: 0.1rem 1rem;
+  width: 4rem;
+  box-shadow: rgba(248,177,121, 0.35) 0px 5px 15px;
+  background: #F8B179;
+
+  @media (min-width: 530px) {
+    width: 7.4rem;
+    padding: 0.2rem 2.4rem;
+  }
+}
 `;
--- src/scss/global/GlobalStyle.jsx
+++ src/scss/global/GlobalStyle.jsx
@@ -28,10 +28,8 @@ const GlobalStyle = createGlobalStyle`
     color: white;
     font-size: 1.6rem;
     min-height: 100vh;
-    // background: linear-gradient(95deg, #B97F00, #6B5628);
-    // background: linear-gradient(95deg, #2D3250, #424669);
     background: #2D3250;
-    margin: 0.5rem;
+    margin: 0rem 0.5rem;
   }
 
   li {
