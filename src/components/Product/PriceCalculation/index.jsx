05ebba7127519c0e92318d75a21320b6ba00bd50
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-17 09:58:08 +0100

    changed to percentage discount

--- src/components/Product/PriceCalculation/index.jsx
+++ src/components/Product/PriceCalculation/index.jsx
@@ -3,16 +3,19 @@ import "../../../scss/styles.scss"
 function ProductPrice({ price, discount }) {
 
   let sale;
+  let discountedPrice;
 
   if (price !== discount) {
-    sale = Math.floor(price - discount);
+    discountedPrice = price - (price * (discount / price));
+    sale = (discountedPrice / price) * 100;
+    console.log(sale);
   } else {
     sale = false;
   }
 
   return (
     <div>
-      <p className={sale ? "sale" : ""}>{price },-</p>{sale && <p>{discount} <span className="sale-color">-{sale},-</span></p>}
+      <p className={sale ? "sale" : ""}>{price },-</p>{sale && <p>{discount} <span className="sale-color"> -{Math.round(sale)}%</span></p>}
     </div>
   );
 }
