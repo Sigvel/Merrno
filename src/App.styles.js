27b5969f67ad01947aca112635e15e5e22ee34fe
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-21 20:20:25 +0100

    responsive styled components

--- src/App.styles.js
+++ src/App.styles.js
@@ -27,12 +27,22 @@ export const PageContainer = styled.div`
     padding: 11rem 1rem 10rem;
     h1 {
       font-size: 2.5rem;
-      inset: auto 0.5rem 7.2rem;
+      inset: auto 0.5rem 7rem;
     }
   }
 
   @media (min-width: 780px) {
     padding: 8rem 1rem 13rem;
+  }
+
+  @media (min-width: 1120px) {
+    display: flex;
+    gap: 1.5rem;
+
+    h1 {
+      font-size: 3rem;
+      inset: auto 0.5rem 0rem;
+    }
   }
 `;
 
@@ -54,9 +64,13 @@ export const Container = styled.div`
   @media (min-width: 900px) {
     grid-template-columns: repeat(4, 1fr);
   }
+
+  @media (min-width: 1120px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
 `;
 
-// Page aside feature Container 
+// Page aside feature Container
 export const AsideFeature = styled.aside`
   display: flex;
   overflow-x: scroll;
@@ -65,9 +79,20 @@ export const AsideFeature = styled.aside`
   position: fixed;
   inset: auto 0.5rem 0;
 
+  .img-box {
+    display: flex;
+    @media (min-width: 1120px) {
+      flex-direction: column;
+    }
+  }
+
   img {
     background: #f8b179;
-    box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+    :hover {
+      box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px;
+      cursor: pointer;
+    }
     padding: 0.5rem;
     margin: 1rem 0.8rem;
     width: 4rem;
@@ -78,12 +103,22 @@ export const AsideFeature = styled.aside`
     img {
       width: 5rem;
     }
+
+    @media (min-width: 1120px) {
+      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      border-radius: 0.5rem;
+      overflow-x: initial;
+      position: initial;
+      .show-btn {
+        display: none;
+      }
+    }
   }
 `;
 
 export const imgContainer = styled.div`
-display: flex;
-gap: 1rem;
+  display: flex;
+  gap: 1rem;
 `;
 
 export const imgBox = styled.img`
@@ -110,12 +145,19 @@ export const Card = styled.div`
 export const Button = styled.button`
   text-transform: uppercase;
   font-size: 1.2rem;
-  padding: 0.3rem 1.5rem;
+  font-weight: var(--font-semibold);
+  padding: 0.5rem 1.5rem;
   border-style: none;
   border-radius: 3rem;
   background: #f8b179;
-  box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+  /* box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px; */
   color: #2d3250;
+  :hover {
+    /* box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset; */
+    box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px;
+    cursor: pointer;
+  }
 `;
 
 export const Breadcrumb = styled.ul`
@@ -135,7 +177,7 @@ export const SearchBar = styled.div`
 
   input[type="search"] {
     width: 100%;
-    padding: 0.25rem 0.7rem;
+    padding: 0.5rem 0.7rem;
     border-radius: 5rem;
     border: none;
     background: #f8b179;
@@ -147,12 +189,13 @@ export const SearchBar = styled.div`
     }
     :focus {
       outline: #2d3250;
+      box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
     }
   }
 
   img {
     border-radius: 5rem;
-    padding: 0.1rem 1rem;
+    padding: 0.25rem 1rem;
     width: 4rem;
     background: #f8b179;
     position: absolute;
@@ -167,9 +210,9 @@ export const SearchBar = styled.div`
     }
 
     img {
-      width: 7.2rem;
-      margin: 0.2rem 1rem;
-      padding: 0.2rem 2.4rem;
+      width: 7.5rem;
+      margin: 0rem 1.1rem;
+      padding: 0.2rem 2.3rem;
       right: 23rem;
     }
   }
@@ -181,7 +224,7 @@ export const SearchIcon = styled.div`
     border-radius: 5rem;
     padding: 0.1rem 1rem;
     width: 4rem;
-    box-shadow: rgba(248, 177, 121, 0.35) 0px 5px 15px;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
     background: #f8b179;
 
     @media (min-width: 530px) {
