09c448981d5be838e21c30065e66f3fca74257e9
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-23 21:04:48 +0100

    added productCart to cart page

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,30 +1,19 @@
+import ProductCart from "../../components/Product/Cart";
+
 import * as S from "../../App.styles";
 import F from "../../scss/modules/pageFeature/Feature.module.scss";
+// import { useLocalStorage } from "../../hooks/useProductStore";
+// import { useStore } from "../../hooks/useProductStore";
 
 function Cart() {
   return (
     <S.PageContainer>
       <h1>Cart</h1>
-    <S.Breadcrumb />
-    <h2>Your cart</h2>
-    <S.Card>
-      <div>
-      <img src="#" alt="product"/>
-      <h3>Title</h3>
-      <p>Description</p>
-      </div>
-      <div>
-      <div>
-        <button type="button">+</button>
-        <button type="button">-</button>
-      </div>
-      <p>price</p>
-      <p>Discount: 22.99,-</p>
-      <button type="button">X</button>
-      </div>
-    </S.Card>
-    <S.AsideFeature className={F.featureButton}>
-      <S.Button type="button">Show form</S.Button>
+      <S.Breadcrumb />
+      <h2>Your cart</h2>
+      <ProductCart />
+      <S.AsideFeature className={F.featureButton}>
+        <S.Button type="button">Show form</S.Button>
       </S.AsideFeature>
     </S.PageContainer>
   );
