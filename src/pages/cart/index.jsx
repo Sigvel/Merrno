2fb0481d492551b2fcfd3d77d4453d72da39d120
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-27 03:43:27 +0200

    form setup and total sum

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -12,35 +12,35 @@ function Cart() {
   return (
     <S.PageContainer>
       <h1>Cart</h1>
-<div className={styles.contentWrap}>
-      <S.Breadcrumb />
-      <section className={styles.cartHero}>
-        <h2>Your cart</h2>
-        <div>
-          <p>Amount</p>
-          <p>Price</p>
-          <p>Delete</p>
-        </div>
-      </section>
+      <div className={styles.contentWrap}>
+        <S.Breadcrumb />
+        <section className={styles.cartHero}>
+          <h2>Your cart</h2>
+          <div>
+            <p>Amount</p>
+            <p>Price</p>
+            <p>Delete</p>
+          </div>
+        </section>
 
-      <ProductCart />
+        <ProductCart />
 
-      <section className={styles.checkout}>
-        
-        <div>total: {Math.round(total)}</div>
-        <S.Button onclick={() => handleCheckout()}>Checkout</S.Button>
-      </section>
+        <section className={styles.checkout}>
+          <div>Total: {Math.round(total)} kr</div>
+          <S.Button onclick={() => handleCheckout()}>Checkout</S.Button>
+        </section>
       </div>
       <S.AsideFeature className={F.featureButton}>
+        <h2>Billing address</h2>
         <form>
-          <label></label>
-          <input name="your-address" type="text" placeholder="Address" />
-          <label></label>
-          <input name="your-address" type="text" placeholder="Address" />
           <label></label>
           <input name="your-email" type="email" placeholder="Email" />
           <label></label>
           <input name="your-name" type="text" placeholder="Full name" />
+          <label></label>
+          <input name="your-address" type="text" placeholder="Address" />
+          <label></label>
+          <input name="your-country-code" type="text" placeholder="Country Code" />
         </form>
         <div>
           <S.Button type="button">Show form</S.Button>
