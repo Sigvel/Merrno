688eae5eb34ae425b815d75037daf71a06672233
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-30 22:36:33 +0200

    success page information update

--- src/pages/success/index.jsx
+++ src/pages/success/index.jsx
@@ -2,34 +2,36 @@ import { useProductStore } from "../../hooks/useProductStore";
 
 import * as S from "../../App.styles";
 import styles from "./Success.module.scss";
-import C from "../../scss/modules/cards/Card.module.scss";
 
 function Success() {
-  const products = useProductStore((state) => state.products);
+  const { products, totalCost } = useProductStore((state) => state.order);
+  const orderNumber = Math.floor(Math.random() * 100000) + 100000;
 
   return (
-<S.PageContainer>
-  <div className={styles.contentWrap}>
-<S.Card className={C.bigCard}>
-<h2>Your purchase was successful</h2>
-</S.Card>
+    <S.PageContainer>
+      <div className={styles.contentWrap}>
+        <h2 className="success">Your order has been received.</h2>
 
-<S.Card>
-<div className={styles.orderSummary}>
-  <h2>Order summary</h2>
-  <h3>Order number: {Math.floor(Math.random() * 10000) + 10000 }</h3>
-  <h4>Products:</h4>
-  {products.map((product) => {
-    return (
-      <li>
-        <h2>{product.title}</h2>
-      </li>
-    )
-  })}
-</div>
-</S.Card>
-</div>
-</S.PageContainer>
+        <div className={styles.orderSummary}>
+          <h3>Order number: {orderNumber}</h3>
+          <div className={styles.ordersWrap}>
+            <h2>Products:</h2>
+            {products.map((product) => {
+              return (
+                <S.Card key={product.id} className={styles.card}>
+                  <h3 className={styles.productTitle}>{product.title}</h3>
+                  <p>quantity: {product.quantity}</p>
+                </S.Card>
+              );
+            })}
+            <p className={styles.total}>Total: {Math.round(totalCost)} kr</p>
+          </div>
+          <div className={styles.buttonWrapper}>
+            <S.Button>Back to shopping</S.Button>
+          </div>
+        </div>
+      </div>
+    </S.PageContainer>
   );
 }
 
