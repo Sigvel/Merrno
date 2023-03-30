69cb07b7964c90541fca304e36c9024bd9bac02e
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-03-30 08:40:04 +0200

    added checkout page to cart

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import ProductCart from "../../components/Product/Cart";
 import { useStore } from "../../hooks/useProductStore";
 
@@ -6,46 +8,51 @@ import styles from "./Cart.module.scss";
 import F from "../../scss/modules/pageFeature/Feature.module.scss";
 
 function Cart() {
-  const { getCartTotal, handleCheckout } = useStore();
+  const { getCartTotal, cartCheckout } = useStore();
   const total = getCartTotal();
 
+  function checkoutButton() {
+    cartCheckout();
+  }
   return (
     <S.PageContainer>
       <h1>Cart</h1>
       <div className={styles.pageWrap}>
-      <div className={styles.contentWrap}>
-        <S.Breadcrumb />
-        <section className={styles.cartHero}>
-          <h2>Your cart</h2>
-          <div>
-            <p>Amount</p>
-            <p>Price</p>
-            <p>Delete</p>
-          </div>
-        </section>
+        <div className={styles.contentWrap}>
+          <S.Breadcrumb />
+          <section className={styles.cartHero}>
+            <h2>Your cart</h2>
+            <div>
+              <p>Amount</p>
+              <p>Price</p>
+              <p>Delete</p>
+            </div>
+          </section>
 
-        <ProductCart />
+          <ProductCart />
 
-        <section className={styles.checkout}>
-          <div>Total: {Math.round(total)} kr</div>
-        </section>
-      </div>
-      <S.AsideFeature className={F.featureButton}>
-        <h2>Billing address</h2>
-        <form>
-          <label></label>
-          <input name="your-email" type="email" placeholder="Email" />
-          <label></label>
-          <input name="your-name" type="text" placeholder="Full name" />
-          <label></label>
-          <input name="your-address" type="text" placeholder="Address" />
-          <label></label>
-          <input name="your-country-code" type="text" placeholder="Country Code" />
-        </form>
-        <div>
-        <S.Button onclick={() => handleCheckout()}>Checkout</S.Button>
+          <section className={styles.checkout}>
+            <div>Total: {Math.round(total)} kr</div>
+          </section>
         </div>
-      </S.AsideFeature>
+        <S.AsideFeature className={F.featureButton}>
+          <h2>Billing address</h2>
+          <form>
+            <label></label>
+            <input name="your-email" type="email" placeholder="Email" />
+            <label></label>
+            <input name="your-name" type="text" placeholder="Full name" />
+            <label></label>
+            <input name="your-address" type="text" placeholder="Address" />
+            <label></label>
+            <input name="your-country-code" type="text" placeholder="Country Code" />
+          </form>
+          <div>
+            <S.Button onClick={() => checkoutButton()}>
+              <Link to={`/success`}>Checkout</Link>
+            </S.Button>
+          </div>
+        </S.AsideFeature>
       </div>
     </S.PageContainer>
   );
